410490756f64886daa9f45c2367964e548d47b0f
Denis <denisqwe41@gmail.com>
2018-03-19 15:53:00 +0300

    Create new component Resume.vue

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,22 +1,38 @@
 <template>
-    <div class="index">
+    <div class="resumes">
         <Navbar
                 :title="$t('pages.summary.navbarTitle')"
                 :isNavigate="true"
                 :isBalance="true"
         />
-
-        Много резюме от исполнителей
+        <section class="main">
+            <div class="content nomenu">
+                <div class="container" v-if="isLoader">
+                    <div class="row">
+                        <div class="col-12">
+                            <Spinner/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
 <script>
     import Navbar from "./layouts/Navbar";
+    import Spinner from "./layouts/Spinner";
 
     export default {
         name: "resume",
         components: {
-            Navbar
+            Navbar,
+            Spinner
+        },
+        data() {
+            return {
+                isLoader: false
+            }
         }
     }
 </script>
