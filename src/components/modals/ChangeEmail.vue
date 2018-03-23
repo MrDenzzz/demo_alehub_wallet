57192d40c7a5311312d614e7ad46c0284b7e2c17
zhiskar <zhiskar.reventlov@gmail.com>
2018-03-23 04:31:58 +0300

    Fix load spinner, add error toasted in ChangeEmail.vue component

--- src/components/modals/ChangeEmail.vue
+++ src/components/modals/ChangeEmail.vue
@@ -2,18 +2,15 @@
     <modal name="changeemail" height="auto" class="modal-xs">
         <div class="heading">
             <p class="title">Change email</p>
-            <i class="close" v-if="dataProcessing" @click="closeModal"></i>
+            <i class="close" @click="closeModal"></i>
         </div>
         <div class="body">
-            <div v-if="dataProcessing" class="wrap-spinner">
-                <Spinner v-if="dataProcessing"/>
-            </div>
-
-            <form v-else @submit.prevent="changeEmail()">
+            <form @submit.prevent="changeEmail()">
                 <div class="modal-control">
                     <div class="modal-input">
                         <label class="title">2fa key</label>
                         <input
+                                id="twoauth-change-email"
                                 type="number"
                                 class="input"
                                 placeholder="Enter 2fa code"
@@ -26,12 +23,17 @@
                     <div class="modal-input">
                         <label class="title">Email</label>
                         <input
+                                id="email-change-email"
                                 type="email"
                                 class="input"
                                 placeholder="Enter new email"
                                 v-model="email"
                                 required>
                     </div>
+                </div>
+
+                <div v-if="dataProcessing" class="wrap-spinner">
+                    <Spinner v-if="dataProcessing" />
                 </div>
 
                 <div class="modal-btn text-center">
@@ -65,17 +67,31 @@
                 this.$modal.hide('changeemail');
             },
             changeEmail: function () {
-                if (this.token.length === 6) {
+                if (this.token.length === 6 && this.email.length !== 0) {
                     this.dataProcessing = true;
+                    document.getElementById('twoauth-change-email').disabled = true;
+                    document.getElementById('email-change-email').disabled = true;
                     const {email, token} = this;
-                    this.$store.dispatch('changeEmail', {email, token}).then(() => {
+                    this.$store.dispatch('changeEmail', {
+                        email, token
+                    }).then(() => {
                         this.email = '';
                         this.token = '';
                         this.dataProcessing = false;
                         this.closeModal();
+                        this.$toasted.show(`Confirmation of the change of email was sent to this address '${this.email}'`, {
+                            duration: 5000,
+                            type: 'success',
+                        });
                         this.$parent.$emit('changeEmail', email);
-                    }).catch(() => {
-                        console.log('You are sent to me wrong email data');
+                    }).catch((err) => {
+                        this.email = '';
+                        this.token = '';
+                        this.$toasted.show(`You are sent to me wrong email data`, {
+                            duration: 10000,
+                            type: 'error',
+                        });
+                        this.dataProcessing = false;
                     });
                 } else {
                     this.email = '';
@@ -105,5 +121,5 @@
     .wrap-spinner
         display flex
         justify-content center
-        padding 0 0 2em 0
+        padding 1em 0
 </style>
