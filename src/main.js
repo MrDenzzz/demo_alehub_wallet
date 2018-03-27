fa7495cad1f1798f4e6353f5ea24bb1951e99596
sergeevagalina <sergeevagalina.al@gmail.com>
2018-03-27 20:01:18 +0300

    Move scss from NewWallet component to static/scss

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ import axios from 'axios';
 import sha256 from 'sha256';
 
 Vue.config.productionTip = false;
-Vue.prototype.$host = 'http://192.168.1.37:4000';
+Vue.prototype.$host = 'http://192.168.1.42:4000';
 // Vue.prototype.$host = 'http://192.168.1.37:4000';
 // Vue.prototype.$host = 'localhost:4000';
 
