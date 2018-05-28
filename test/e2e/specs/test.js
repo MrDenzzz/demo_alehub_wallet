39af3269faad06f58d07c6d622e7a0edc0540d6b
im <zhitkov100@gmal.com>
2018-05-28 12:52:35 +0300

    test e2e

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -2,19 +2,82 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-    // let sas = ['c', 12, 0, -1, ''];
-    // for (let i = 0; i++; i < sas.length){
-  'default autharisation test': function (browser) {
-        browser
-            .url('http://localhost:8080')
-            .waitForElementVisible('#app', 5000)
-            .setValue('input[name="password"]', -1)
-            .setValue('input[name="login"]', '')
+
+
+    before : function(client) {
+        client.globals.waitForConditionTimeout = 5000;
+    },
+
+  'default autharisation test': function (client) {
+      client
+          .url('http://localhost:8080')
+          .waitForElementVisible('#app', 5000)
+          .setValue('input#password', "-1")
+          .setValue('input#email', "-1")
+          .click('button[name="submit"]')
+          .waitForElementVisible('.error.toasted', 1000)
+          .verify.containsText('.error.toasted', "Enter your valid email");
+
+
+      client
+          .clearValue('input#password')
+          .setValue('input#password', "3453sssssss4")
+          .clearValue('input#email')
+          .setValue('input#email', 'zhitkov100@gmail.com')
+          .click('button[name="submit"]')
+          .waitForElementVisible('.error.toasted', 5000)
+          .verify.containsText('.error.toasted', "Enter your valid password");
+
+      client
+          .clearValue('input#password')
+          .setValue('input#password', "34534")
+          .clearValue('input#email')
+          .setValue('input#email', 'zhitkov100@gmail.com')
+          .click('button[name="submit"]')
+          .waitForElementVisible('.error.toasted', 5000)
+          .verify.containsText('.error.toasted', "The password must be at least 8 characters in length");
+  },
+'wallets': function (client) {
+
+      client
+            .clearValue('input#email')
+            .setValue('input#email', 'zhitkov100@gmail.com')
+            .clearValue('input#password')
+            .setValue('input#password', 'qweqweqwe')
             .click('button[name="submit"]')
-            .waitForElementVisible('.toasted', 1000)
-            .assert.containsText('.toasted', "Enter your valid email")
+            .waitForElementVisible('.toasted', 5000)
+            .verify.containsText('.toasted', "Autharizathion access")
+      client
+            .waitForElementVisible('div.wallet', 5000)
+          .waitForElementVisible('div[id="7LR41yw1OapLoYmxjYhXYpAdwyB9nMHbxpSScQOkYdL14KR"]')
+            .click('div[id="7LR41yw1OapLoYmxjYhXYpAdwyB9nMHbxpSScQOkYdL14KR"]')
+            .click('button.buttons.btn-default')
+            .waitForElementVisible('.v--modal-box.v--modal', 5000)
+            .click('button#btnCopyAddress')
+          .timeoutsImplicitWait(2000)
+            .waitForElementVisible('.success', 5000)
+            .verify.containsText('.success', "You have successfully copied the address of the wallet");
 
+    client
+            .waitForElementVisible('div.wallet', 5000)
+            .timeoutsImplicitWait(2000)
+            .click('div[id="w8LgnPbbwow26XUYg65zucnGVRxJCf6antc6HbSHnOLDUgZ]"')
+            .click('button.buttons.btn-yellow')
+            .waitForElementVisible('.v--modal-box.v--modal', 5000)
+            .setValue('input#amount', "0-12")
+            // .click('input#address')
+            .setValue('input#address', function(){
+                document.querySelector('div').addEventListener('paste', (e) => {
+                    console.log(e);
+                    window.setTimeout(() => {
 
-            .end();
-    }
+                    });
+                });
+             });
+    client
+        .pause(1000)
+},
+    after : function(client) {
+        client.end();
+    },
 };
