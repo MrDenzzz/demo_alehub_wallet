6ebd7abb97a2de96a088e7fdccf846cb47653e8f
zhiskar <zhiskar.reventlov@gmail.com>
2018-07-09 18:40:09 +0300

    Add separators to OffersControlLine.vue, Add OffersDatepicker.vue

--- src/components/layouts/Offers/OffersControlLine.vue
+++ src/components/layouts/Offers/OffersControlLine.vue
@@ -3,6 +3,10 @@
         <offers-control-new-offer/>
         <offers-control-toggle-filter/>
         <offers-control-toggle-state/>
+        <div class="divider-container">
+            <div class="divider"
+                 v-for="n in 12" :key="n"></div>
+        </div>
     </div>
 </template>
 
@@ -28,4 +32,16 @@
         width 35px
         height 550px
         background-color #e7eaee
+
+        .divider-container
+            display flex
+            flex-direction column
+            justify-content space-between
+            align-items center
+            height 100%
+
+            .divider
+                width 30px
+                height 1px
+                background-color #ffffff
 </style>
---
+++ src/components/layouts/Offers/OffersDatepicker.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="offers-datepicker">
+        <datepicker></datepicker>
+    </div>
+</template>
+
+<script>
+    import Datepicker from 'vuejs-datepicker';
+
+    export default {
+        name: 'OffersDatepicker'
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
