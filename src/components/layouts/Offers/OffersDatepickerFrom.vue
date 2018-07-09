4698208f0f7321d4890d6d3025fe95603f2b4508
zhiskar <zhiskar.reventlov@gmail.com>
2018-07-09 18:43:37 +0300

    Rename OffersDatepicker.vue to OffersDatepickerFrom.vue, Add OffersDatepicker.vue

---
+++ src/components/layouts/Offers/OffersDatepickerFrom.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="offers-datepicker">
+        <datepicker id="dateOffersFrom"
+                    class="dateOffersFrom"
+                    v-if="openedFromDatepicker"
+                    v-model="offersDateFrom"
+                    :language="$t('modals.pdf.lang')"
+                    :inline="inline"/>
+    </div>
+</template>
+
+<script>
+    import Datepicker from 'vuejs-datepicker';
+
+    export default {
+        name: 'OffersDatepicker',
+        data() {
+            return {
+                inline: true,
+                openedFromDatepicker: false,
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
