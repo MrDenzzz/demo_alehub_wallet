61f6afee0833a8904a68e15a711be5ccefa09805
zhiskar <zhiskar.reventlov@gmail.com>
2018-07-23 17:40:54 +0300

    Add OfferContractorList.vue, OfferContractorItem.vue components

---
+++ src/components/layouts/offers/OfferContractorList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="offer-contractor-list">
+
+    </div>
+</template>
+
+<script>
+    import OfferContractorItem from './OfferContractorItem';
+
+    export default {
+        name: 'OfferContractorList',
+        components: {
+            OfferContractorItem
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
