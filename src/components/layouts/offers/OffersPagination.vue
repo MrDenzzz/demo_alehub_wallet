97f0a57398ae25c2a6bd9759810aa11de5dd1168
zhiskar <zhiskar.reventlov@gmail.com>
2018-08-07 14:20:53 +0300

    Add OffersPagination.vue component

---
+++ src/components/layouts/offers/OffersPagination.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="offers-pagination">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'OffersPagination'
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
